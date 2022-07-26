69a62cfe45780b9937c419402d050c9272be7fae
markcaf <marcocafassog@gmail.com>
2022-07-27 01:40:47 +0200

    style: add of Oswald font as main of the project

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,12 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap.scss";
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Oswald', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
