5a1d833c725b2c1c05a6a06c2c7e39cc9a2f42ac
markcaf <marcocafassog@gmail.com>
2022-07-28 01:32:58 +0200

    feat(pizzalist): add pizza card component w/ info

---
+++ src/components/PizzaCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pizzacard text-center">
+    <img src="../assets/img/h3-product-img-3a-150x150.png" alt="">
+    <h5 class="title text-uppercase mt-4 mb-2">Nome Pizza</h5>
+    <div class="price">$50.00</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PizzaCard',
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+    .pizzacard{
+        width: 200px;
+        img{
+            width: 100%
+        };
+
+        .title{
+            color: $pizza_title;
+        }
+
+        .price{
+            color: $pizza_price;
+            font-size: 18px;
+            font-weight: 500;
+        }
+    }
+
+</style>
