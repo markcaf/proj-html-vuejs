bf4646c211cfec04a38aad61f364527c666b8ecc
markcaf <marcocafassog@gmail.com>
2022-07-28 00:31:13 +0200

    feat: creation of a component for the partner logo

---
+++ src/components/LogoPartner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="logo_partner text-center">
+        <img src="../assets/img/h1-clients-img-3.png" alt="logo partner">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LogoPartner',
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .logo_partner{
+        width: 180px;
+        img{
+            width: 100%;
+        }
+    }
+
+</style>
