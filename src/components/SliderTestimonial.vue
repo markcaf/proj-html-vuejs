cc750ecd83e9d705638c0af65b09215bc65136c0
markcaf <marcocafassog@gmail.com>
2022-07-27 13:25:25 +0200

    feat: creation of static testimonial's slider comp

---
+++ src/components/SliderTestimonial.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="my-5 d-flex justify-content-between align-items-center">
+    <div class="prev-button">
+        Prev
+    </div>
+
+    <div class="content text-center">
+        <div class="quote">“</div>
+        <div class="testimonial_text">
+            “Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque reiciendis repudiandae officia”
+        </div>
+        <div class="testimonial_author">
+            Washington post 2018
+        </div>
+
+        <div class="navigation">
+            <span class="dot active">
+                <img src="../assets/svg/svg-5.svg" width="8" alt="">
+            </span>
+            <span class="dot">
+                <img src="../assets/svg/svg-5.svg" width="8" alt="">
+            </span>
+            <span class="dot">
+                <img src="../assets/svg/svg-5.svg" width="8" alt="">
+            </span>
+        </div>
+    </div>
+
+    <div class="next-button">
+        Next
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'SliderTestimonial',
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+    section{
+        height: 300px;
+        background-image: url(../assets/img/h3-testimonials-bckgrnd.jpg);
+        background-color: $testimonial_bg;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+
+        .content{
+            max-width: 600px;
+
+            .quote{
+                font-size: 150px;
+                color: $brown_text;
+                font-weight: 500;
+                line-height: 20px;
+            }
+
+            .testimonial_text{
+                color: #303030;
+                text-transform: uppercase;
+                font-weight: 500;
+                font-size: 25px;
+            }
+
+            .testimonial_author{
+                color: $btn_slider_text;
+                font-size: 12px;
+                font-weight: 500;
+                text-transform: uppercase;
+            }
+
+            .navigation{
+                .dot{
+                    filter: opacity(0.2);
+                    margin: 0 5px;
+
+                    &.active{
+                        filter: invert(52%) sepia(69%) saturate(382%) hue-rotate(3deg) brightness(97%) contrast(89%);
+                    }
+                }
+            }
+        }
+
+        .prev-button{
+            writing-mode: vertical-rl;
+            background-color: white;
+            color: $btn_slider_text;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            padding-right: 5px;
+            border-radius: 0 30px 30px 0;
+        }
+
+        .next-button{
+            writing-mode: vertical-lr;
+            background-color: white;
+            color: $btn_slider_text;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            padding-right: 5px;
+            border-radius: 0 30px 30px 0;
+            -moz-transform: scale(-1, -1);
+            -webkit-transform: scale(-1, -1);
+            -o-transform: scale(-1, -1);
+            -ms-transform: scale(-1, -1);
+            transform: scale(-1, -1);
+        }
+    }
+
+</style>
