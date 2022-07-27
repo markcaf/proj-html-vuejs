869c6879c7a5fe3d7fe8dfe914cdb2c8c557c8c8
markcaf <marcocafassog@gmail.com>
2022-07-27 11:27:52 +0200

    feat(main): add section with 4 image cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,34 @@
 <template>
   <main>
-    
+    <section id="image_cards" class="d-flex justify-content-between">
+        <img src="../assets/img/h3-img-1.jpg" alt="">
+        <img src="../assets/img/h3-img-1.jpg" alt="">
+        <img src="../assets/img/h3-img-1.jpg" alt="">
+        <img src="../assets/img/h3-img-1.jpg" alt="">
+    </section>
+
   </main>
 </template>
 
 <script>
+
 export default {
     name: 'Main',
+    components: {
+
+}
 
 }
 </script>
 
 <style lang="scss" scoped>
-
+@import "../styles/variables.scss";
+main{
+    #image_cards{
+        img{
+            width: calc((100% / 4) - 8px);
+            margin: 8px 0;
+        }
+    }
+}
 </style>
