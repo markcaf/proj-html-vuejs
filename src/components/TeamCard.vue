4a6b7968246f132390d261d1a4fba5227014dbd3
markcaf <marcocafassog@gmail.com>
2022-07-28 00:12:26 +0200

    feat: creation of a component for the team card

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="team_card">
+    <img src="../assets/img/h1-team-2a.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TeamCard',
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .team_card{
+        img{
+            width: 100%;
+        }
+    }
+
+</style>
