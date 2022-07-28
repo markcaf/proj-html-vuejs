fcfd331c7728e30433a1f454ba46c9883824f6df
markcaf <marcocafassog@gmail.com>
2022-07-28 13:54:52 +0200

    refactor(imageCard): dynamic generation with props

---
+++ src/components/main_components/imageCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="imgUrl" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'imageCard',
+
+    props: {
+        imgUrl: String,
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+    img{
+        width: 100%;
+        margin: 8px 0;
+    }
+</style>
