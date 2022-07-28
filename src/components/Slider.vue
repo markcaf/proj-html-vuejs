9c59a9a6f895d0ff1fd92a64e3cee08ef7141214
markcaf <marcocafassog@gmail.com>
2022-07-28 17:53:49 +0200

    feat(slider): add dynamic slider, change on click

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,14 +1,15 @@
 <template>
   <section class="my-5 d-flex justify-content-between align-items-center">
-    <div class="prev-button">
+    <div class="prev-button" @click="previousPicture">
         Prev
     </div>
 
     <div class="content text-center">
-        <img src="../assets/img/h3-rev-img-6.png" alt="">
+        <!-- <img src="../assets/img/h3-rev-img-6.png" alt=""> -->
+        <img :src="slides[activeImageIndex].image" :alt="Pizza" />
     </div>
 
-    <div class="next-button">
+    <div class="next-button" @click="nextPicture">
         Next
     </div>
   </section>
@@ -17,6 +18,53 @@
 <script>
 export default {
     name: 'Slider',
+
+    data: function(){
+        return{
+            activeImageIndex: 0,
+
+            autoPlay: null,
+
+            slides: [
+                {
+                    image: require('@/assets/img/h3-rev-img-6.png'),
+                    bg: require('@/assets/img/h3-rev-img-5.png'),
+                },
+
+                {
+                    image: require('@/assets/img/h3-rev-img-4.png'),
+                    bg: require('@/assets/img/h3-rev-img-3.png'),
+                },
+
+                {
+                    image: require('@/assets/img/h3-rev-img-2.png'),
+                    bg: require('@/assets/img/h3-rev-img-1.png'),
+                }, 
+            ],
+        }
+    },
+
+    methods: {
+        nextPicture: function () {
+                if (this.activeImageIndex === this.slides.length - 1) {
+                    this.activeImageIndex = 0;
+                } else {
+                    this.activeImageIndex++;
+                };
+            },
+
+        previousPicture: function () {
+            if (this.activeImageIndex === 0) {
+                this.activeImageIndex = this.slides.length - 1;
+            } else {
+                this.activeImageIndex--;
+            };
+        },
+
+        changePictureToIndex: function( indexToBeChanged ){
+            this.activeImageIndex = indexToBeChanged;
+        },
+    }
 
 }
 </script>
@@ -45,6 +93,7 @@ export default {
             padding-bottom: 20px;
             padding-right: 5px;
             border-radius: 0 30px 30px 0;
+            cursor: pointer;
         }
 
         .next-button{
@@ -60,6 +109,7 @@ export default {
             -o-transform: scale(-1, -1);
             -ms-transform: scale(-1, -1);
             transform: scale(-1, -1);
+            cursor: pointer;
         }
     }
 
