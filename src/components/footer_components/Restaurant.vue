0711da8eefd160bf710ddd44307579797758dc36
markcaf <marcocafassog@gmail.com>
2022-07-28 16:21:39 +0200

    feat(restaurant): dynamic gen of infos w/ props

---
+++ src/components/footer_components/Restaurant.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <div class="address">{{address}}</div>
+        <div class="city">{{city}}</div>
+        <div class="tel">{{tel}}</div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Restaurant',
+
+    props: {
+        address: String,
+        city: String,
+        tel: String,
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/variables.scss";
+
+    li{
+        color: $p_secondary;
+        font-family: sans-serif;
+        margin: 20px 0;
+    }
+
+</style>
