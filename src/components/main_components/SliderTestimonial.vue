5d0cc49df82e13ada912b2bdfb5c49deda6734a6
markcaf <marcocafassog@gmail.com>
2022-07-28 18:00:39 +0200

    feat(testimonial): add dynamic slider with quotes

--- src/components/main_components/SliderTestimonial.vue
+++ src/components/main_components/SliderTestimonial.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="my-5 d-flex justify-content-between align-items-center">
-    <div class="prev-button">
+    <div class="prev-button" @click="previousPicture">
         Prev
     </div>
 
     <div class="content text-center">
         <div class="quote">“</div>
         <div class="testimonial_text">
-            “Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque reiciendis repudiandae officia”
+            {{slides[activeImageIndex].text}}
         </div>
         <div class="testimonial_author">
-            Washington post 2018
+            {{slides[activeImageIndex].author}}
         </div>
 
         <div class="navigation">
@@ -26,7 +26,7 @@
         </div>
     </div>
 
-    <div class="next-button">
+    <div class="next-button" @click="nextPicture">
         Next
     </div>
   </section>
@@ -35,6 +35,53 @@
 <script>
 export default {
     name: 'SliderTestimonial',
+
+    data: function(){
+        return{
+            activeImageIndex: 0,
+
+            autoPlay: null,
+
+            slides: [
+                {
+                    text: 'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISICING ELIT. QUISQUAM EAQUE REICIENDIS REPUDIANDAE OFFICIA',
+                    author: 'washington post 2018',
+                },
+
+                {
+                    text: '“QUISQUAM EAQUE REICIENDIS REPUDIANDAE OFFICIA”',
+                    author: 'los angeles post 2018',
+                },
+
+                {
+                    text: '“LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISICING ELIT.”',
+                    author: 'ny post 2018',
+                }, 
+            ],
+        }
+    },
+
+    methods: {
+        nextPicture: function () {
+                if (this.activeImageIndex === this.slides.length - 1) {
+                    this.activeImageIndex = 0;
+                } else {
+                    this.activeImageIndex++;
+                };
+            },
+
+        previousPicture: function () {
+            if (this.activeImageIndex === 0) {
+                this.activeImageIndex = this.slides.length - 1;
+            } else {
+                this.activeImageIndex--;
+            };
+        },
+
+        changePictureToIndex: function( indexToBeChanged ){
+            this.activeImageIndex = indexToBeChanged;
+        },
+    },
 
 }
 </script>
@@ -94,6 +141,7 @@ export default {
             padding-bottom: 20px;
             padding-right: 5px;
             border-radius: 0 30px 30px 0;
+            cursor: pointer;
         }
 
         .next-button{
@@ -109,6 +157,7 @@ export default {
             -o-transform: scale(-1, -1);
             -ms-transform: scale(-1, -1);
             transform: scale(-1, -1);
+            cursor: pointer;
         }
     }
 
