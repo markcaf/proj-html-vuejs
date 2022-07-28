fd75d8a6c07a0b3d66fab18f2195e08966f025f0
markcaf <marcocafassog@gmail.com>
2022-07-28 16:51:39 +0200

    style(imagecard): add hover on card, bg and icon

--- src/components/main_components/imageCard.vue
+++ src/components/main_components/imageCard.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="wrapper">
     <img :src="imgUrl" alt="">
+    <div class="icon_hover text-center">
+      <i class="fa-regular fa-eye"></i>
+    </div>
   </div>
 </template>
 
@@ -16,8 +19,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../styles/variables.scss";
+
+  .wrapper{
+    position: relative;
+    transition: 0.5s ease-in-out; 
+
+    &:hover{
+      transform: scale(0.9);
+      transition: transform 0.7s;
+      background-image: url(../../assets/img/background-hover-dotted.jpeg);
+      background-size: contain;
+
+      img{
+        opacity: 0;
+        position: relative;
+      }
+
+      .icon_hover{
+        display: block;
+      }
+    }
+
     img{
         width: 100%;
         margin: 8px 0;
     }
+
+    .icon_hover{
+      position: absolute;
+      top: 40%;
+      left: 42%;
+      display: none;
+      
+      .fa-eye{
+      font-size: 30px;
+      padding: 10px;
+      border-radius: 50%;
+      background-color: $red_bg;
+      color: white;
+      }
+    }
+  }
 </style>
